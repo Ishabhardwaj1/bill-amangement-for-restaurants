59ce873ee62c18d7c876e9a4a0e22eec3b928037
Ishabhardwaj1 <pksharma347@gmail.com>
2023-11-10 13:19:45 +0530

    first message

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+
+@section('content')
+<div>
+	<div class="row justify-content-center">
+        <div class="col-md-8">
+			<div class="card">
+				<div class="card-body">
+					<h5 class="card-title">How can i manage my Bills ?</h5>
+					<p class="card-text">
+						with my bill management you can:
+						<ul>
+							<li> Manage your products</li>
+							<li> Keep track of the unpaid bills </li>
+						</ul>
+					</p>
+		 		</div>
+			</div>
+ 		</div>
+	</div>
+</div>
+@endsection
